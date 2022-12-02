0fa674c4b0fab99ab4fe6dea2e4cf2c5d9cde689
Carlo Sebastian La Porta <carloalaple@gmail.com>
2022-12-02 23:37:28 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+let itemsData = [
+	{
+		"name": "The Lord of the Rings",
+		"price": "50.50",
+		"category": "books",
+		"image": "https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/lotr.jpg"
+	},
+	{
+		"name": "Game of Thrones",
+		"price": "25.00",
+		"category": "books",
+		"image": "https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/got.jpg"
+	},
+	{
+		"name": "Chocolate bar",
+		"price": "9.99",
+		"category": "food",
+		"image": "https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/cb.jpg"
+	},
+	{
+		"name": "Box of chocolates",
+		"price": "14.99",
+		"category": "food",
+		"image": "https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/boc.jpg"
+	},
+	{
+		"name": "Headache pills",
+		"price": "15.00",
+		"category": "medical-products",
+		"image": "https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/mmt.png"
+	},
+	{
+		"name": "Music CD",
+		"price": "10.00",
+		"category": "other",
+		"image": "https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/cd.png"
+	},
+	{
+		"name": "Bottle of perfume",
+		"price": "50.50",
+		"category": "other",
+		"image": "https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/c5.png"
+	}
+];
--- main.js
+++ main.js
@@ -1,23 +1,51 @@
-fetch('https://instilla-sales-tax-problem.s3.eu-central-1.amazonaws.com/sales-tax-problem-test.json')
-.then((response) => response.json())
-.then((data) => {
-    // console.log(data["products"]);
-    for (i = 0; i < data["products"].length; i++) {
-        // console.log(data["products"][i]["id"]);
-        temp = document.createElement('div');
-        temp.className = 'results';
-        temp.innerHTML = '<div class="card">'+
+let itemsContainer = document.getElementById("items-container");
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+let generateShop = () => {
+   return (itemsContainer.innerHTML = itemsData.map((x) => {
+       let {name, price, category,image} = x;
+       let search = basket.find((x) => x.id === id) || [];
+       return `
 
-        '<div class="imgBox">'+
-         ' <img src="'+data["products"][i]["images"][0]+'" alt="mouse corsair" class="mouse">'+
-       ' </div>'+
-      
-       ' <div class="contentBox">'+
-         ' <h3>'+data["products"][i]["title"]+'</h3>'+
-         ' <h2 class="price">'+data["products"][i]["price"]+'.<small>0'+data["products"][i]["id"]+'</small> Afg</h2>'+
-         ' <a href="#" class="buy">Buy Now</a>'+
-      
-     ' </div>';
-        document.getElementsByClassName('container')[0].appendChild(temp);
-      }
-  });
+
+       <div class="item">
+       <div class="image-wrapper">
+           <img class="item-image" src="${image}" alt="">
+           <div class="badge-secondary"><img src="assets/badge-icon.svg" alt=""><span class="badge-category">${category}</span></div>
+       </div>
+       <div class="itemDetails">
+         <h4> ${name} </h4>
+       <span class="price"> $${price} </span>
+       <label class="import-wrapper"> Apply import duty
+           <input type="checkbox" >
+           <div class="checkmark"><svg class="check" width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <path fill-rule="evenodd" clip-rule="evenodd" d="M2.63484 4.76755L0.98486 3.24002L0.276367 3.89594L2.63484 6.07938L7.49859 1.57657L6.7901 0.920654L2.63484 4.76755Z" fill="white"/>
+             </svg>
+             </div>
+       </label>
+       <button><span>add to cart</span></button>
+       </div>
+     </div>
+
+     `;
+     })
+     .join(""));
+ };
+  /*     <div id=product-id-${category} class="item">
+         <img width="220" src=${image} alt="">
+         <div class="details">
+           <h3>${name}</h3>
+           <p>${category}</p>
+           <div class="price-quantity">
+             <h2>$ ${price} </h2>
+             <div class="buttons">
+               <i onclick="decrement(${name})" class="bi bi-dash-lg"></i>
+               <div id=${name} class="quantity">
+               ${search.item === undefined ? 0 : search.item}
+               </div>
+               <i onclick="increment(${name})" class="fa-solid fa-plus-lg"></i>
+             </div>
+           </div>
+         </div>
+       </div>
+ */
+ generateShop();
